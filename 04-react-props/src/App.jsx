851b04c5cbfdbfa87a215f16e850900b11a5f8c7
Rodrigo Bossini <professorbossini@gmail.com>
2025-10-22 11:04:32 -0300

    learn(ap04-react): encerra secao 2.8
    - retomar terminando a apostila
    - comecar a apostila 07
    - obs: nesta versao, fizemos a definicao dos pedidos numa lista

--- 04-react-props/src/App.jsx
+++ 04-react-props/src/App.jsx
@@ -1,6 +1,41 @@
 import Pedido from "./Pedido"
-
+import Cartao from "./Cartao"
 const App = () => {
+
+  const pedidos = [
+    {
+      data: '22/05/2025',
+      categoriaIcone: 'solid',
+      icone: 'hard-drive',
+      titulo: 'SSD',
+      descricao: 'Um SSD'
+
+    },
+    {
+      data: '21/09/2025',
+      categoriaIcone: 'solid',
+      icone: 'book',
+      titulo: 'Um livro',
+      descricao: 'Concrete Maths'
+
+    },
+    {
+      data: '22/09/2025',
+      categoriaIcone: 'solid',
+      icone: 'anchor',
+      titulo: 'Âncora',
+      descricao: 'Uma âncora'
+
+    },{
+      data: '21/10/2025',
+      categoriaIcone: 'solid',
+      icone: 'fish',
+      titulo: 'Peixe',
+      descricao: 'Um peixe'
+
+    }
+  ]
+
   return (
     <div className="container border rounded mt-2">
       
@@ -11,38 +46,24 @@ const App = () => {
       </div>
 
       <div className="row">
-        <div className="col-sm-12 col-lg-6 col-xl-4 col-xxl-3">
-          <Pedido 
-            data='20/05/2025'
-            categoriaIcone='solid'
-            icone='hard-drive'
-            titulo='SSD'
-            descricao='SSD de 1 Terabyte'/>
-        </div>
-        <div className="col-sm-12 col-lg-6 col-xl-4 col-xxl-3">
-          <Pedido 
-            data='22/07/2025'
-            categoriaIcone='solid'
-            icone='book'
-            titulo='Livro'
-            descricao='Concrete Maths'/>
-        </div>
-        <div className="col-sm-12 col-lg-6 col-xl-4 col-xxl-3">
-          <Pedido 
-            data='21/09/2025'
-            categoriaIcone='solid'
-            icone='anchor'
-            titulo='Âncora'
-            descricao='Uma âncora'/>
-        </div>
-        <div className="col-sm-12 col-lg-6 col-xl-4 col-xxl-3">
-          <Pedido 
-            data='22/05/2025'
-            categoriaIcone='solid'
-            icone='fish'
-            titulo='Peixe'
-            descricao='Um peixe'/> 
-        </div>  
+        {
+          pedidos.map((p, index) => (
+            <div className="col-sm-12 col-lg-6 col-xl-4 col-xxl-3">
+
+              <Cartao
+                cabecalho={p.data}>
+                <Pedido               
+                  categoriaIcone={p.categoriaIcone}
+                  icone={p.icone}
+                  titulo={p.titulo}
+                  descricao={p.descricao}/>
+              </Cartao>
+
+            </div>
+          ))
+        }
+
+        
       </div>
     </div>
   )
---
+++ 04-react-props/src/Cartao.jsx
@@ -0,0 +1,17 @@
+// rafce
+import React from 'react'
+
+const Cartao = ({cabecalho, children}) => {
+  return (
+    <div className='card'>
+      <div className="card-header text-muted">
+        {cabecalho}
+      </div>
+      <div className="card-body">
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default Cartao
