4024f63fde0f778459e0a550f326300ca7993cca
Rodrigo Bossini <professorbossini@gmail.com>
2025-10-29 12:49:06 -0300

    learn(ap07-react): encerra secao 2.6
    - retomar fazendo requisicoes
    - chave esta no front
    - fazer back end a seguir

--- 07-react-exibe-fotos/src/components/App.jsx
+++ 07-react-exibe-fotos/src/components/App.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import Busca from './Busca'
 
-const App = () => {
-  return (
-    <div className='grid border-1 border-400 border-round-sm p-3 mx-3 mt-3'>
-      <div className="col-12">
-        <i className="pi pi-truck"></i>
+class App extends React.Component {
+
+  onBuscaRealizada = (termoDeBusca) => {
+    const chaveAPI = 'a91Qyfh2Ud1rdeOGKV8aTR5Aj9UmRvdma6EdyhC9EfKStoAyt7rmDuhV'
+    console.log(termoDeBusca)
+  }
+  render(){
+    return (
+      <div className='grid border-1 border-400 border-round-sm p-3 mx-3 mt-3'>
+        <div className="col-12">
+          <i className="pi pi-truck"></i>
+        </div>
+        <div className="col-12">
+          <Busca 
+            onBuscaRealizada={this.onBuscaRealizada}
+            dica="Digite o que quer ver"/>
+        </div>
       </div>
-      <div className="col-12">
-        <Busca />
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App
--- 07-react-exibe-fotos/src/components/Busca.jsx
+++ 07-react-exibe-fotos/src/components/Busca.jsx
@@ -1,17 +1,67 @@
 import React, { Component } from 'react'
+import { Button } from 'primereact/button'
 import { IconField } from 'primereact/iconfield'
 import { InputIcon } from 'primereact/inputicon'
 import { InputText } from 'primereact/inputtext'
 
+// const f = (props) => {}
+// class Pessoa{
+//   String nome;
+// }
+
+// class Aluno extends Pessoa{
+//   void exibirNome(){
+//     print(nome);
+//   }
+// }
 export default class Busca extends Component {
+
+  // props = null
+  // constructor(props){
+  //   super(props)
+  //   const { onBuscaRealizada } = 
+  // }
+
+  state = {
+    termoDeBusca: ''
+  }
+
+  onTermoAlterado = (evento) => {
+    console.log(evento.target.value)
+    //não faça assim
+    // this.state.termoDeBusca = evento.target.value
+    this.setState({
+      termoDeBusca: evento.target.value
+    })
+    // this.render()
+  }
+
+  onFormSubmit = (evento) => {
+    evento.preventDefault()
+    this.props.onBuscaRealizada(this.state.termoDeBusca)  
+  }
+
   render() {
     return (
-      <div className='flex flex-column w-full'>
-        <IconField iconPosition='left' className='w-full'>
-          <InputIcon className='pi pi-search'/>
-          <InputText className='w-full' placeholder='Buscar...' />
-        </IconField>
-      </div>
+      <form onSubmit={this.onFormSubmit}>
+        <div className='flex flex-column w-full'>
+          <IconField iconPosition='left' className='w-full'>
+            <InputIcon className='pi pi-search'/>
+            <InputText
+              value={this.state.termoDeBusca} 
+              className='w-full' 
+              placeholder={this.props.dica}
+              onChange={this.onTermoAlterado} />
+          </IconField>
+          <Button 
+            label='OK'
+            className='mt-2 p-button-outlined'/>
+        </div>
+      </form>
     )
   }
+}
+
+Busca.defaultProps = {
+  dica: 'Buscar...'  
 }
