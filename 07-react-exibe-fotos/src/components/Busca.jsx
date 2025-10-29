ff625f9a74add7de79812ed3e86f1176b94cf64f
Rodrigo Bossini <professorbossini@gmail.com>
2025-10-29 11:03:23 -0300

    learn(ap07-react): encerra secao 2.6
    - retomar fazendo as requisicoes
    - lembrar de isolar a chave num back end no futuro

--- 07-react-exibe-fotos/src/components/Busca.jsx
+++ 07-react-exibe-fotos/src/components/Busca.jsx
@@ -1,17 +1,49 @@
 import React, { Component } from 'react'
+import { Button } from 'primereact/button'
 import { IconField } from 'primereact/iconfield'
 import { InputIcon } from 'primereact/inputicon'
 import { InputText } from 'primereact/inputtext'
 
 export default class Busca extends Component {
+
+  state = {
+    termoDeBusca: ''
+  }
+  
+  onTermoAlterado = (evento) => {
+    console.log(evento.target.value)
+    this.setState({
+      termoDeBusca: evento.target.value
+    })
+  }
+
+  onFormSubmit = (evento) => {
+    evento.preventDefault()
+    this.props.onBuscaRealizada(this.state.termoDeBusca)
+
+  }
   render() {
     return (
-      <div className='flex flex-column w-full'>
-        <IconField iconPosition='left' className='w-full'>
-          <InputIcon className='pi pi-search'/>
-          <InputText className='w-full' placeholder='Buscar...' />
-        </IconField>
-      </div>
+      <form onSubmit={this.onFormSubmit}>
+        <div className='flex flex-column w-full'>
+          <IconField iconPosition='left' className='w-full'>
+            <InputIcon className='pi pi-search'/>
+            <InputText
+              value={this.state.termoDeBusca} 
+              className='w-full' 
+              placeholder={this.props.dica}
+              onChange={this.onTermoAlterado} />
+          </IconField>
+          <Button 
+            label='OK'
+            className='p-button-outlined mt-2'
+            />
+        </div>
+      </form>
     )
   }
+}
+
+Busca.defaultProps = {
+  dica: 'Buscar...'
 }
